6178d6ce58302ffe7ecceb8f91fdc3e1d5940b27
youngTaekVue <dudxor129@gmail.com>
2023-01-19 23:08:02 +0900

    Composition test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,62 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+<!--
+Here we are reactively binding element attributes / properties to the state.
+The :title syntax is short for v-bind:title.
+-->
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+const isRed = ref(true)
+const color = ref('green')
+
+function toggleRed() {
+  isRed.value = !isRed.value
+}
+
+function toggleColor() {
+  color.value = color.value === 'green' ? 'blue' : 'green'
+}
+</script>
+
+- <template>
+  <div class="q-pa-md row items-start q-gutter-md">
+  
+  <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+  </p>
+
+  <!--
+  class bindings have special support for objects and arrays
+  in addition to plain strings
+  -->
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+
+  <!-- style bindings also support object and arrays -->
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p> 
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
     >
-  </q-page>
+      <q-card-section>
+        <div class="text-h6">{{message}}</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+ 
+    </q-card>
+ 
+  </div> 
+ 
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script>
+<style>
+.red {
+  color: red;
+}
+</style>
